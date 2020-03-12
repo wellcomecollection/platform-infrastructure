dc659366d3a6080ad04d4855b8a7483829254793
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-03-12 08:13:36 +0000

    make assumable role MaxSessionDuration 4 hours

--- accounts/modules/roleset/main.tf
+++ accounts/modules/roleset/main.tf
@@ -3,6 +3,7 @@ module "role" {
   name                = var.name
   federated_principal = var.federated_principal
   aws_principal       = var.aws_principal
+  max_session_duration_in_seconds = var.max_session_duration_in_seconds
 }
 
 module "role_policy" {
--- accounts/modules/roleset/variables.tf
+++ accounts/modules/roleset/variables.tf
@@ -5,3 +5,9 @@ variable "aws_principal" {}
 variable "assumable_role_arns" {
   type = list(string)
 }
+
+variable "max_session_duration_in_seconds" {
+  type = number
+  # 1 hour
+  default = 60 * 60
+}
