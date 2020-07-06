8e21a26734ceafa47ce5a385eb702e8d73c0c9c8
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-07-06 14:05:48 +0100

    Rename containers folder images

--- images/terraform/locals.tf
+++ images/terraform/locals.tf
@@ -4,7 +4,8 @@ locals {
     "760097843905", // platform_account_id
     "975596993436", // storage_account_id
     "299497370133", // workflow_account_id
-    "130871440101"  // experience_account_id
+    "130871440101", // experience_account_id
+    "653428163053"  // digirati_account_id
   ]
 
   namespace = "uk.ac.wellcome"
