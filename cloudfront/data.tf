2723507692afac11747f0753fbc7bf6eee9c0e14
Robert Kenny <kenoir@gmail.com>
2020-07-24 15:38:17 +0100

    incorporate productboard

--- cloudfront/data.tf
+++ cloudfront/data.tf
@@ -21,3 +21,9 @@ data "aws_acm_certificate" "logging_wc_org" {
   statuses = ["ISSUED"]
   provider = aws.us_east_1
 }
+
+data "aws_acm_certificate" "raodmap_wc_org" {
+  domain   = "roadmap.wellcomecollection.org"
+  statuses = ["ISSUED"]
+  provider = aws.us_east_1
+}
---
+++ cloudfront/productboard/variables.tf
@@ -0,0 +1,11 @@
+variable "alias" {
+  type = string
+}
+
+variable "comment" {
+  type = string
+}
+
+variable "acm_certificate_arn" {
+  type = string
+}
