bebfc7d16ce097c11d55edc0caa53f7fb5eb962c
Robert Kenny <kenoir@gmail.com>
2020-09-07 14:59:57 +0100

    catalogue chatbot

---
+++ monitoring/terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "catalogue_chatbot_role_arn" {
+  value = aws_iam_role.catalogue_chatbot.arn
+}
--- monitoring/terraform/provider.tf
+++ monitoring/terraform/provider.tf
@@ -8,6 +8,17 @@ provider "aws" {
 }
 
 provider "aws" {
+  alias = "catalogue"
+
+  region  = "eu-west-1"
+  version = "2.35.0"
+
+  assume_role {
+    role_arn = "arn:aws:iam::756629837203:role/catalogue-admin"
+  }
+}
+
+provider "aws" {
   region = "us-east-1"
   alias  = "us_east_1"
 
