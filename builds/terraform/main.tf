a9d1856d948d109fc7e307c8ff28d49dfe3a424b
Robert Kenny <kenoir@gmail.com>
2020-09-24 17:37:08 +0100

    Add Buildkite stack to Terraform

--- builds/terraform/main.tf
+++ builds/terraform/main.tf
@@ -1,5 +1,78 @@
+# TODO: Switch to ci-agent (allow devs to assume new role)
+
+resource "aws_cloudformation_stack" "buildkite" {
+  name = "buildkite-elasticstack"
+
+  capabilities = ["CAPABILITY_NAMED_IAM"]
+
+  parameters = {
+    BuildkiteAgentToken =  data.aws_secretsmanager_secret_version.example.secret_string
+
+    MinSize = 0
+    MaxSize = 1
+
+    ScaleDownPeriod = 300
+    ScaleCooldownPeriod = 60
+
+    SpotPrice = 0.04
+
+    ScaleUpAdjustment = 1
+    ScaleDownAdjustment = -10
+
+    AgentsPerInstance = 1
+    BuildkiteTerminateInstanceAfterJobTimeout = 1800
+
+    RootVolumeSize = 250
+    RootVolumeName = "/dev/xvda"
+    RootVolumeType = "gp2"
+
+    InstanceType = "r5.large"
+    InstanceCreationTimeout = "PT5M"
+    InstanceRoleName = "ci-agent"
+
+    VpcId = "vpc-0a5a53b1949a23989" # ci-172-43-0-0-16
+    Subnets = "subnet-017f5da7c9c90971a,subnet-0c9c03f2ca40a6e97,subnet-0e1adc0bbbeae116a" # private subnets
+
+    AssociatePublicIpAddress = true
+
+    KeyName = "wellcomedigitalplatform"
+
+    CostAllocationTagName = "aws:createdBy"
+    CostAllocationTagValue = "buildkite-elasticstack"
+
+    BuildkiteQueue = "default"
+    BuildkiteAgentRelease = "stable"
+    BuildkiteAgentTimestampLines = false
+    BuildkiteTerminateInstanceAfterJob = true
+    BuildkiteTerminateInstanceAfterJobDecreaseDesiredCapacity = true
+
+    EnableExperimentalLambdaBasedAutoscaling = true
+    EnableECRPlugin = true
+    EnableSecretsPlugin = true
+    EnableDockerLoginPlugin = true
+    EnableCostAllocationTags = false
+    EnableDockerExperimental = false
+    EnableAgentGitMirrorsExperiment = false
+    EnableDockerUserNamespaceRemap = false
+
+  }
+
+  template_body = file("${path.module}/buildkite.yaml")
+}
+
+data "aws_iam_role" "ci_agent" {
+  name = "ci-agent"
+}
+
+resource "aws_iam_role_policy" "ci_agent" {
+  policy = data.aws_iam_policy_document.ci_permissions.json
+  role = data.aws_iam_role.ci_agent.id
+
+  provider = aws
+}
+
 data "aws_iam_role" "buildkite_agent" {
-  name = "buildkite-agent"
+  name = local.buildkite_role_name
 }
 
 resource "aws_iam_role_policy" "buildkite_agent" {
