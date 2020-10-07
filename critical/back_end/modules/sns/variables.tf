0a61554753ec003903bc6a1b515a4bd476d8d5c5
Alex Chan <alex@alexwlchan.net>
2020-10-07 07:37:03 +0100

    Use the versioned SNS module, not a vendored version

--- critical/back_end/modules/sns/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  description = "Name of the SNS topic"
-}
-
-variable "cross_account_subscription_ids" {
-  type        = list(string)
-  default     = []
-  description = "AWS account IDs alolowed to subscribe to this topic"
-}
--- critical/back_end/topics.tf
+++ critical/back_end/topics.tf
@@ -1,24 +1,24 @@
 module "lambda_error_alarm" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "lambda_error_alarm"
 }
 
 # Alarm topics
 
 module "dlq_alarm" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "shared_dlq_alarm"
 }
 
 module "gateway_server_error_alarm" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "shared_gateway_server_error_alarm"
 
   cross_account_subscription_ids = [local.account_ids["catalogue"]]
 }
 
 module "terraform_apply_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "shared_terraform_apply"
 }
 
@@ -27,18 +27,18 @@ module "terraform_apply_topic" {
 ## Reporting
 
 module "reporting_miro_reindex_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "reporting_miro_reindex_topic"
   cross_account_subscription_ids = [local.account_ids["reporting"]]
 }
 
 module "reporting_sierra_reindex_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "reporting_sierra_reindex_topic"
 }
 
 module "reporting_miro_inventory_reindex_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "reporting_miro_inventory_reindex_topic"
   cross_account_subscription_ids = [local.account_ids["reporting"]]
 }
@@ -46,30 +46,30 @@ module "reporting_miro_inventory_reindex_topic" {
 ## Catalogue
 
 module "catalogue_miro_reindex_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "catalogue_miro_reindex_topic"
 }
 
 module "catalogue_sierra_reindex_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "catalogue_sierra_reindex_topic"
 }
 
 module "catalogue_sierra_items_reindex_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "catalogue_sierra_items_reindex_topic"
 }
 
 ## Inference
 
 module "inference_calm_reindex_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "inference_calm_reindex_topic"
 }
 
 # Shared topics for updates to VHS source data
 module "miro_updates_topic" {
-  source = "./modules/sns"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "vhs_sourcedata_miro_updates_topic"
   cross_account_subscription_ids = [local.account_ids["reporting"]]
 }
