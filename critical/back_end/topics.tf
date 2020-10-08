eea4a65bbadef8d489818bfbced371741772559e
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2020-10-08 06:35:51 +0000

    Apply auto-formatting rules

--- critical/back_end/topics.tf
+++ critical/back_end/topics.tf
@@ -27,8 +27,8 @@ module "terraform_apply_topic" {
 ## Reporting
 
 module "reporting_miro_reindex_topic" {
-  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
-  name   = "reporting_miro_reindex_topic"
+  source                         = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
+  name                           = "reporting_miro_reindex_topic"
   cross_account_subscription_ids = [local.account_ids["reporting"]]
 }
 
@@ -38,8 +38,8 @@ module "reporting_sierra_reindex_topic" {
 }
 
 module "reporting_miro_inventory_reindex_topic" {
-  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
-  name   = "reporting_miro_inventory_reindex_topic"
+  source                         = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
+  name                           = "reporting_miro_inventory_reindex_topic"
   cross_account_subscription_ids = [local.account_ids["reporting"]]
 }
 
@@ -69,7 +69,7 @@ module "inference_calm_reindex_topic" {
 
 # Shared topics for updates to VHS source data
 module "miro_updates_topic" {
-  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
-  name   = "vhs_sourcedata_miro_updates_topic"
+  source                         = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
+  name                           = "vhs_sourcedata_miro_updates_topic"
   cross_account_subscription_ids = [local.account_ids["reporting"]]
 }
