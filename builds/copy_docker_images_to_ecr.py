457629b24e424dd318c09d5bd9ea4ba3a3066ba5
Alex Chan <a.chan@wellcome.org>
2020-11-17 17:19:07 +0000

    Tidy up the script for mirroring Docker Hub images to ECR

--- builds/copy_docker_images_to_ecr.py
+++ builds/copy_docker_images_to_ecr.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python
 """
-Docker Hub enforces rate limits for unauthenticated users, and our build system
-will hit those limits.
+Docker Hub is starting to introduce rate limits for anonymous users. [1]
 
-This script mirrors the images we care about into ECR repositories in
-our AWS accounts, so we're not affected by those rate limits.
+We pull images from Docker Hub in our CI setup.  We were starting to hit the
+rate limits in CI (partly because we run a lot of parallel workers, partly because
+our tests pull a lot of different images).
+
+This script mirrors images from Docker Hub to repositories in ECR inside
+our AWS account.
+
+Our CI workers are EC2 instances that run inside the same AWS account, so our CI
+can pull images from ECR instead of Docker Hub without having to pay AWS egress
+charges or hitting Docker Hub rate limits.
+
+If you want to use this script to mirror images from Docker Hub to ECR:
+
+*   Get some local AWS credentials, so that ``boto3.client("ecr")`` returns
+    an ECR client authenticated against the account you want to mirror images to
+*   Put your own account ID in ``ACCOUNT_ID``
+*   Replace the list ``IMAGE_TAGS`` with the tags of every image you want
+    to mirror
+
+Wellcome users: run this script as
+
+    AWS_PROFILE=platform-dev python3 copy_docker_images_to_ecr.py
+
+[1]: https://www.docker.com/blog/what-you-need-to-know-about-upcoming-docker-hub-rate-limiting/
+
 """
 
-import functools
+import base64
 import subprocess
 
 import boto3
-import click
+from botocore.exceptions import ClientError
 
 
-IMAGES = [
+ACCOUNT_ID = "760097843905"
+
+IMAGE_TAGS = [
     "hashicorp/terraform:light",
     "library/mysql:5.6",
     "localstack/localstack",
@@ -27,85 +51,116 @@ IMAGES = [
     "wellcome/fake-sns",
     "wellcome/flake8:latest",
     "wellcome/format_python:112",
+    "wellcome/format_python:latest",
     "wellcome/image_builder:23",
     "wellcome/publish_lambda:130",
     "wellcome/sbt_wrapper",
     "wellcome/scalafmt:edge",
+    "wellcome/scalafmt:latest",
+    "wellcome/weco-deploy:5.5.3",
     "wellcome/weco-deploy:5.5.7",
+    "wellcome/weco-deploy:5.6.4",
     "zenko/cloudserver:8.1.8",
 ]
 
-ACCOUNTS = {"760097843905": "platform"}
-
-
-def docker(*args):
-    subprocess.check_call(["docker"] + list(args))
-
 
 def print(msg):
-    click.echo(click.style(f"*** {msg}", "blue"))
+    # Print in green to make messages stand out.  Based on termcolor.
+    import builtins
+
+    builtins.print(f"\x1b[34m*** {msg}\x1b[0m")
 
 
-@functools.lru_cache()
-def get_aws_client(resource, *, role_arn):
-    assumed_role_object = boto3.client("sts").assume_role(
-        RoleArn=role_arn, RoleSessionName="AssumeRoleSession1"
-    )
-    credentials = assumed_role_object["Credentials"]
-    return boto3.client(
-        resource,
-        aws_access_key_id=credentials["AccessKeyId"],
-        aws_secret_access_key=credentials["SecretAccessKey"],
-        aws_session_token=credentials["SessionToken"],
-    )
-
-
-def ensure_ecr_repos_exist(ecr_client, *, account_id, account_name, image_tags):
+def get_ecr_repo_names_in_account(ecr_client, *, account_id):
     """
-    Ensure we have ECR image repositories that we can mirror images into.
+    Returns a set of all the ECR repository names in an AWS account.
     """
-    known_repos = set()
+    repo_names = set()
 
     paginator = ecr_client.get_paginator("describe_repositories")
     for page in paginator.paginate(registryId=account_id):
         for repo in page["repositories"]:
-            known_repos.add(repo["repositoryName"])
+            repo_names.add(repo["repositoryName"])
 
-    for tag in image_tags:
-        name = tag.split(":")[0]  # eg hashicorp/terraform:light ~> hashicorp/terraform
-        if name not in known_repos:
-            ecr_client.create_repository(repositoryName=name)
+    return repo_names
+
+
+def docker_login_to_ecr(ecr_client, *, account_id):
+    """
+    Authenticate Docker against the ECR repository in a particular account.
+
+    The authorization token obtained from ECR is good for twelve hours, so this
+    function is cached to save repeatedly getting a token and running `docker login`
+    in quick succession.
+    """
+    response = ecr_client.get_authorization_token(registryIds=[account_id])
+
+    try:
+        auth = response["authorizationData"][0]
+    except (IndexError, KeyError):
+        raise RuntimeError("Unable to get authorization token from ECR!")
+
+    auth_token = base64.b64decode(auth["authorizationToken"]).decode()
+    username, password = auth_token.split(":")
+
+    cmd = [
+        "docker",
+        "login",
+        "--username",
+        username,
+        "--password",
+        password,
+        auth["proxyEndpoint"],
+    ]
+
+    proc = subprocess.check_call(cmd)
+
+
+def create_ecr_repository(ecr_client, *, name):
+    """
+    Create a new ECR repository.
+    """
+    try:
+        ecr_client.create_repository(repositoryName=name)
+    except ClientError as err:
+        if err.response["Error"]["Code"] == "RepositoryAlreadyExistsException":
+            pass
+        else:
+            raise
+
+
+def docker(*args):
+    """
+    Shell out to the Docker CLI.
+    """
+    subprocess.check_call(["docker"] + list(args))
+
+
+def mirror_docker_hub_images_to_ecr(ecr_client, *, account_id, image_tags):
+    """
+    Given the name/tag of images in Docker Hub, mirror those images to ECR.
+    """
+    print("Creating all ECR repositories...")
+    existing_repos = get_ecr_repo_names_in_account(ecr_client, account_id=account_id)
+
+    mirrored_repos = set(tag.split(":")[0] for tag in image_tags)
+    missing_repos = mirrored_repos - existing_repos
+
+    for repo_name in missing_repos:
+        ecr_client.create_repository(repositoryName=repo_name)
+
+    print("Authenticating Docker with ECR...")
+    docker_login_to_ecr(ecr_client, account_id=account_id)
+
+    for hub_tag in image_tags:
+        ecr_tag = f"{account_id}.dkr.ecr.eu-west-1.amazonaws.com/{hub_tag}"
+        print(f"Mirroring {hub_tag} to {ecr_tag}")
+        docker("pull", hub_tag)
+        docker("tag", hub_tag, ecr_tag)
+        docker("push", ecr_tag)
 
 
 if __name__ == "__main__":
-    for account in ACCOUNTS.values():
-        print(f"Logging in to ECR in the {account} account")
-        subprocess.check_call(
-            f"eval $(AWS_PROFILE={account}-dev aws ecr get-login --no-include-email)",
-            shell=True,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-
-    for account_id, account_name in ACCOUNTS.items():
-        print(f"Creating ECR repos in the {account} account")
-
-        role_arn = f"arn:aws:iam::{account_id}:role/{account_name}-publisher"
-        ecr_client = get_aws_client("ecr", role_arn=role_arn)
-
-        ensure_ecr_repos_exist(
-            ecr_client,
-            account_id=account_id,
-            account_name=account_name,
-            image_tags=IMAGES,
-        )
-
-    for image_tag in IMAGES:
-        print(f"Pulling {image_tag}")
-        docker("pull", image_tag)
-
-        for account_id, account_name in ACCOUNTS.items():
-            ecr_image_tag = f"{account_id}.dkr.ecr.eu-west-1.amazonaws.com/{image_tag}"
-            print(f"Pushing {image_tag} to {ecr_image_tag} ({account_name})")
-            docker("tag", image_tag, ecr_image_tag)
-            docker("push", ecr_image_tag)
+    mirror_docker_hub_images_to_ecr(
+        ecr_client=boto3.client("ecr"), account_id=ACCOUNT_ID, image_tags=IMAGE_TAGS
+    )
