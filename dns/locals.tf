e29b646ec5a67c3d592cb65e97105fa945986640
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2020-11-25 13:19:19 +0000

    Apply auto-formatting rules

--- dns/locals.tf
+++ dns/locals.tf
@@ -7,7 +7,7 @@ locals {
   // want to allow the outputs from that stack to set those details here in order
   // to more tightly control of wellcomecollection.org records in this stack.
   identity_ses_txt_records = data.terraform_remote_state.identity.outputs.wellcomecollection_org_ses_vertification_token[["records"]]
-  identity_dkim_cname = data.terraform_remote_state.identity.outputs.wellcomecollection_org_ses_dkim_tokens
+  identity_dkim_cname      = data.terraform_remote_state.identity.outputs.wellcomecollection_org_ses_dkim_tokens
   identity_ses_dkim_records = toset([
     for record in local.identity_dkim_cname : split(".", record["name"])[0]
   ])
