c868c71bea972899ec81d7d1132448da47217e7a
Robert Kenny <kenoir@gmail.com>
2021-01-22 13:49:34 +0000

    allow digirati devs to assume experience roles

--- accounts/platform/rolesets.tf
+++ accounts/platform/rolesets.tf
@@ -340,6 +340,11 @@ module "digirati_dev_roleset" {
     local.identity_account_roles["read_only_role_arn"],
     local.identity_account_roles["ci_role_arn"],
 
+    # Identity
+    local.experience_account_roles["developer_role_arn"],
+    local.experience_account_roles["read_only_role_arn"],
+    local.experience_account_roles["ci_role_arn"],
+
     # Workflow
     local.workflow_account_roles["read_only_role_arn"],
 
