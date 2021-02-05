84e7cd8d12cf9fe466b3fa9d165581b8beaeddf3
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-02-05 13:59:31 +0000

    Apply auto-formatting rules

--- accounts/modules/vpc/public-private-igw/gateway_endpoint/variables.tf
+++ accounts/modules/vpc/public-private-igw/gateway_endpoint/variables.tf
@@ -2,6 +2,6 @@ variable "service" {}
 variable "vpc_id" {}
 variable "route_table_id" {}
 variable "service_type" {
-  type = string
+  type    = string
   default = "Gateway"
 }
--- cloudfront/s3.tf
+++ cloudfront/s3.tf
@@ -2,5 +2,5 @@ resource "aws_s3_bucket" "edge_lambdas" {
   provider = aws.us_east_1
 
   bucket = "wellcomecollection-edge-lambdas"
-  acl = "private"
+  acl    = "private"
 }
