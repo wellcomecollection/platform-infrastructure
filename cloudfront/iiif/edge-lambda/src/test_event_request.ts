da6f755c74928dd6ccc6934db8bad3464a2793f4
Robert Kenny <kenoir@gmail.com>
2021-02-05 14:25:06 +0000

    comment where the example event came from

--- cloudfront/iiif/edge-lambda/src/test_event_request.ts
+++ cloudfront/iiif/edge-lambda/src/test_event_request.ts
@@ -1,5 +1,7 @@
 import { CloudFrontRequestEvent } from 'aws-lambda';
 
+// This event structure was sourced from the AWS docs as below.
+// https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html#example-origin-request
 const request = (
     uri: string,
     querystring?: string
