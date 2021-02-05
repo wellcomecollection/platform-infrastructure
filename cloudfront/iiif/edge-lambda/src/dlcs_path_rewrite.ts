429e982bee63661ec9eca79cf55c5cb376a7867e
Robert Kenny <kenoir@gmail.com>
2021-02-05 14:23:35 +0000

    regex always need comment

--- cloudfront/iiif/edge-lambda/src/dlcs_path_rewrite.ts
+++ cloudfront/iiif/edge-lambda/src/dlcs_path_rewrite.ts
@@ -5,6 +5,7 @@ import {
 export const request: CloudFrontRequestHandler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
 
+    // This regex removes a starting "/image", and optionally a folllowing ".jpg" (with any case)
     request.uri = request.uri.replace(/^\/image\/([A-Za-z0-9]+)(?:\.[Jj][Pp][Gg])?(\/.*)/gm, '/$1$2')
 
     callback(null, request);
