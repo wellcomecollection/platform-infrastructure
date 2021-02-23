1a968697169d51c2a4cecb3b5930eb207e89eeb2
Robert Kenny <kenoir@gmail.com>
2021-02-23 17:10:56 +0000

    Update CloudFront certificate module

--- cloudfront/logging.wellcomecollection.org/cert.tf
+++ cloudfront/logging.wellcomecollection.org/cert.tf
@@ -9,8 +9,16 @@ module "cert" {
 
   ttl = 30
 
+  zone_id = data.aws_route53_zone.zone.id
+
   providers = {
     aws     = aws.us_east_1
     aws.dns = aws.dns
   }
+}
+
+data "aws_route53_zone" "zone" {
+  provider = aws.dns
+
+  name = "wellcomecollection.org."
 }
--- cloudfront/roadmap.wellcomecollection.org/cert.tf
+++ cloudfront/roadmap.wellcomecollection.org/cert.tf
@@ -3,8 +3,16 @@ module "cert" {
 
   domain_name = "roadmap.wellcomecollection.org"
 
+  zone_id = data.aws_route53_zone.zone.id
+
   providers = {
     aws     = aws.us_east_1
     aws.dns = aws.dns
   }
+}
+
+data "aws_route53_zone" "zone" {
+  provider = aws.dns
+
+  name = "wellcomecollection.org."
 }
