bdc675d351fb0be8f6d769e0e9cc56f7fe2acf25
Robert Kenny <kenoir@gmail.com>
2021-02-24 15:08:29 +0000

    Manage Wellcome Library DNS in Terraform

--- cloudfront/wellcomelibrary.org/terraform/cf_origins.tf
+++ cloudfront/wellcomelibrary.org/terraform/cf_origins.tf
@@ -1,16 +1,16 @@
 locals {
   prod_origins = [
     {
-      origin_id : "example"
-      domain_name : "example.com"
+      origin_id : "origin"
+      domain_name : "origin.wellcomecollection.org"
       origin_path : null
     }
   ]
 
   stage_origins = [
     {
-      origin_id : "example"
-      domain_name : "example.com"
+      origin_id : "origin"
+      domain_name : "origin.wellcomecollection.org"
       origin_path : null
     }
   ]
