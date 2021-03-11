456cb18b59211519e2b113833d66672ca4720154
Robert Kenny <kenoir@gmail.com>
2021-03-11 14:17:58 +0000

    hardcode prod version of passthru lambda

--- cloudfront/wellcomelibrary.org/terraform/locals.tf
+++ cloudfront/wellcomelibrary.org/terraform/locals.tf
@@ -11,7 +11,7 @@ locals {
   wellcome_library_passthru_arn_latest = "${local.wellcome_library_passthru_arn}:${local.wellcome_library_passthru_latest}"
   wellcome_library_passthru_arn_stage  = local.wellcome_library_passthru_arn_latest
   # This should be set manually when a stable prod deploy is established.
-  wellcome_library_passthru_arn_prod = local.wellcome_library_redirect_arn_prod
+  wellcome_library_passthru_arn_prod = "${local.wellcome_library_passthru_arn}:1"
 
   edge_lambdas_bucket = data.terraform_remote_state.cloudfront_core.outputs.edge_lambdas_bucket
 }
