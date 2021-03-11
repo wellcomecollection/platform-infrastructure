65e68530dfb82243146f0f9cdb46c81b735f27bf
Robert Kenny <kenoir@gmail.com>
2021-03-11 15:06:19 +0000

    push into prod

--- cloudfront/wellcomelibrary.org/terraform/cf_behaviours.tf
+++ cloudfront/wellcomelibrary.org/terraform/cf_behaviours.tf
@@ -1,5 +1,22 @@
 locals {
-  prod_behaviours = []
+  prod_behaviours = [
+    {
+      path_pattern     = "events*"
+      target_origin_id = "origin"
+      headers          = []
+      cookies          = "all"
+      lambdas = [
+        {
+          event_type = "origin-request"
+          lambda_arn = local.wellcome_library_redirect_arn_prod
+        }
+      ]
+
+      min_ttl     = null
+      default_ttl = null
+      max_ttl     = null
+    }
+  ]
   stage_behaviours = [
     {
       path_pattern     = "item/*"
