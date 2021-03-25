ab6135a052b6efff433f5b3a97f562746429bb43
Robert Kenny <kenoir@gmail.com>
2021-03-25 13:35:29 +0000

    fix stable redirect lambda versions in stage to reduce tf churn

--- cloudfront/wellcomelibrary.org/terraform/locals.tf
+++ cloudfront/wellcomelibrary.org/terraform/locals.tf
@@ -9,16 +9,16 @@ locals {
   wellcome_library_passthru_arn        = aws_lambda_function.wellcome_library_passthru.arn
   wellcome_library_passthru_latest     = aws_lambda_function.wellcome_library_passthru.version
   wellcome_library_passthru_arn_latest = "${local.wellcome_library_passthru_arn}:${local.wellcome_library_passthru_latest}"
-  wellcome_library_passthru_arn_stage  = local.wellcome_library_passthru_arn_latest
+  wellcome_library_passthru_arn_stage  = "${local.wellcome_library_passthru_arn}:1"
   # This should be set manually when a stable prod deploy is established.
   wellcome_library_passthru_arn_prod = "${local.wellcome_library_passthru_arn}:1"
 
   wellcome_library_blog_redirect_arn        = aws_lambda_function.wellcome_library_blog_redirect.arn
   wellcome_library_blog_redirect_latest     = aws_lambda_function.wellcome_library_blog_redirect.version
   wellcome_library_blog_redirect_arn_latest = "${local.wellcome_library_blog_redirect_arn}:${local.wellcome_library_blog_redirect_latest}"
-  wellcome_library_blog_redirect_arn_stage  = local.wellcome_library_blog_redirect_arn_latest
+  wellcome_library_blog_redirect_arn_stage  = "${local.wellcome_library_blog_redirect_arn}:12"
   # This should be set manually when a stable prod deploy is established.
-  wellcome_library_blog_redirect_arn_prod = local.wellcome_library_blog_redirect_arn_stage
+  wellcome_library_blog_redirect_arn_prod = "${local.wellcome_library_blog_redirect_arn}:12"
 
   edge_lambdas_bucket = data.terraform_remote_state.cloudfront_core.outputs.edge_lambdas_bucket
 }
--- cloudfront/wellcomelibrary.org/terraform/outputs.tf
+++ cloudfront/wellcomelibrary.org/terraform/outputs.tf
@@ -9,3 +9,7 @@ output "wellcome_library_blog_redirect_arn_latest" {
 output "wellcome_library_passthru_arn_latest" {
   value = local.wellcome_library_passthru_arn_latest
 }
+
+output "wellcome_library_redirect_lambda_s3_object_version" {
+  value = data.aws_s3_bucket_object.wellcome_library_redirect.version_id
+}
