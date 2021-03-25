98ddf97d99c06fa63524d6caac49add47dc6d6af
Robert Kenny <kenoir@gmail.com>
2021-03-25 12:36:03 +0000

    update DNS entry

--- cloudfront/wellcomelibrary.org/terraform/blog.tf
+++ cloudfront/wellcomelibrary.org/terraform/blog.tf
@@ -40,8 +40,10 @@ resource "aws_route53_record" "blog-prod" {
   zone_id = data.aws_route53_zone.zone.id
   name    = "blog.wellcomelibrary.org"
   type    = "CNAME"
-  records = ["bloglibrary.wpengine.com"]
-  ttl     = "300"
+  // This is the previous on-premise address, if required for easy roll-back
+  //records = ["bloglibrary.wpengine.com"]
+  records = [module.wellcomelibrary_blog-prod.distro_domain_name]
+  ttl     = "60"
 
   provider = aws.dns
 }
