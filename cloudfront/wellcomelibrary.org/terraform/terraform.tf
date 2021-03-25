166bc6bc7a404682fb62784ae49c071f2d232503
Robert Kenny <kenoir@gmail.com>
2021-03-25 10:02:51 +0000

    use role_arn

--- cloudfront/wellcomelibrary.org/terraform/terraform.tf
+++ cloudfront/wellcomelibrary.org/terraform/terraform.tf
@@ -2,13 +2,12 @@ terraform {
   required_version = ">= 0.9"
 
   backend "s3" {
+    role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
+
     bucket         = "wellcomecollection-platform-infra"
     key            = "terraform/platform-infrastructure/cloudfront/wl.org.tfstate"
     dynamodb_table = "terraform-locktable"
-
-    profile = "platform"
-    #role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
-    region = "eu-west-1"
+    region         = "eu-west-1"
   }
 }
 
