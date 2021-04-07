06cbed1977566ed61ce5508679c76d02f698c9c0
Donald Gray <donald.gray@digirati.com>
2021-04-07 14:03:21 +0100

    Add scripts for invalidation lambda

--- cloudfront/invalidation/lambda/src/cache_invalidation.ts
+++ cloudfront/invalidation/lambda/src/cache_invalidation.ts
@@ -1,14 +1,15 @@
 import { SNSHandler } from "aws-lambda/trigger/sns";
-import * as aws from "aws-sdk";
+import { CloudFront } from "aws-sdk";
+import { CreateInvalidationRequest } from "aws-sdk/clients/cloudfront";
 
 export const handler: SNSHandler = async (event) => {
   const distro = process.env.DISTRIBUTION_ID;
 
   const paths = JSON.parse(event.Records[0].Sns.Message);
 
-  const cloudfront = new aws.CloudFront();
-  const params = {
-    DistributionId: distro,
+  const cloudfront = new CloudFront();
+  const params: CreateInvalidationRequest = {
+    DistributionId: String(distro),
     InvalidationBatch: {
       CallerReference: `${Date.now()}`,
       Paths: {
