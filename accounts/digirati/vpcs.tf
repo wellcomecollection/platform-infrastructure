4df66d5365b4f167e2294a32091fd3e64fc7302f
Donald Gray <donald.gray@digirati.com>
2021-05-06 11:20:46 +0100

    Add VPC for MoH migration

--- accounts/digirati/vpcs.tf
+++ accounts/digirati/vpcs.tf
@@ -7,3 +7,13 @@ module "digirati_vpc" {
   vpc_name   = "iiif-services"
   cidr_block = "172.56.0.0/16"
 }
+
+# Used by:
+# - London's Pulse: Medical Officer of Health reports
+
+module "moh_vpc" {
+  source = "../modules/vpc"
+
+  vpc_name   = "moh"
+  cidr_block = "172.57.0.0/16"
+}
