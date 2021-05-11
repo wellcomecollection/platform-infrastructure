50cd2c881b371c855d0efac239a66fb1be16759b
Robert Kenny <kenoir@gmail.com>
2021-05-11 12:55:47 +0100

    Add MoH paths

--- cloudfront/wellcomelibrary.org/terraform/cf_behaviours.tf
+++ cloudfront/wellcomelibrary.org/terraform/cf_behaviours.tf
@@ -101,7 +101,88 @@ locals {
     }
   ]
 
+  moh_behaviours = [
+    {
+      path_pattern     = "moh/*"
+      target_origin_id = local.wellcome_library_moh_origin.origin_id
+      headers          = []
+      cookies          = "all"
+      lambdas          = []
+
+      min_ttl     = null
+      default_ttl = null
+      max_ttl     = null
+    },
+    {
+      path_pattern     = "spas/*"
+      target_origin_id = local.wellcome_library_moh_origin.origin_id
+      headers          = []
+      cookies          = "all"
+      lambdas = [
+        {
+          event_type = "origin-request"
+          lambda_arn = local.wellcome_library_passthru_arn_prod
+        }
+      ]
+
+      min_ttl     = null
+      default_ttl = null
+      max_ttl     = null
+    },
+    {
+      path_pattern     = "assets/*"
+      target_origin_id = local.wellcome_library_moh_origin.origin_id
+      headers          = []
+      cookies          = "all"
+      lambdas          = []
+
+      min_ttl     = null
+      default_ttl = null
+      max_ttl     = null
+    },
+    {
+      path_pattern     = "plugins/*"
+      target_origin_id = local.wellcome_library_moh_origin.origin_id
+      headers          = []
+      cookies          = "all"
+      lambdas = [
+        {
+          event_type = "origin-request"
+          lambda_arn = local.wellcome_library_passthru_arn_prod
+        }
+      ]
+
+      min_ttl     = null
+      default_ttl = null
+      max_ttl     = null
+    },
+    {
+      path_pattern     = "scripts/*"
+      target_origin_id = local.wellcome_library_moh_origin.origin_id
+      headers          = []
+      cookies          = "all"
+      lambdas          = []
+
+      min_ttl     = null
+      default_ttl = null
+      max_ttl     = null
+    },
+    {
+      path_pattern     = "timelines/*"
+      target_origin_id = local.wellcome_library_moh_origin.origin_id
+      headers          = []
+      cookies          = "all"
+      lambdas          = []
+
+      min_ttl     = null
+      default_ttl = null
+      max_ttl     = null
+    }
+  ]
+
   prod_behaviours = []
 
-  stage_behaviours = []
+  stage_behaviours = concat(
+    local.moh_behaviours,
+  )
 }
--- cloudfront/wellcomelibrary.org/terraform/cf_origins.tf
+++ cloudfront/wellcomelibrary.org/terraform/cf_origins.tf
@@ -6,11 +6,19 @@ locals {
     origin_protocol_policy : "match-viewer"
   }
 
+  wellcome_library_moh_origin = {
+    origin_id : "moh_origin"
+    domain_name : "moh.wellcomecollection.digirati.io"
+    origin_path : null
+    origin_protocol_policy : "match-viewer"
+  }
+
   prod_origins = [
     local.wellcome_library_origin
   ]
 
   stage_origins = [
-    local.wellcome_library_origin
+    local.wellcome_library_origin,
+    local.wellcome_library_moh_origin
   ]
 }
