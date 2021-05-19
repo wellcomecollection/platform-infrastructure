c11b498c9dd6e1ddad78e0b4303828ab5f4f8b28
Robert Kenny <kenoir@gmail.com>
2021-05-19 15:03:38 +0100

    Remove unused snapshots infra

--- critical/user_facing/outputs.tf
+++ critical/user_facing/outputs.tf
@@ -13,21 +13,3 @@ output "cognito_storage_api_identifier" {
 output "admin_cidr_ingress" {
   value = local.admin_cidr_ingress
 }
-
-# Elasticcloud
-
-output "elasticcloud_access_id" {
-  value = module.elasticcloud-catalogue.elasticcloud_access_id
-}
-
-output "elasticcloud_access_secret" {
-  value = module.elasticcloud-catalogue.elasticcloud_access_secret
-}
-
-output "elasticcloud_readonly_role_arn" {
-  value = module.elasticcloud-catalogue.elasticcloud_readonly_role_arn
-}
-
-output "elasticcloud_readonly_role_name" {
-  value = module.elasticcloud-catalogue.elasticcloud_readonly_role_name
-}
