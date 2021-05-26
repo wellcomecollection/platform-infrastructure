43ee368f271bca9569dfe9a33e8f123fb1827467
jamie <j.parkinson@wellcome.ac.uk>
2021-05-26 15:15:53 +0100

    Separate prod and stage VPCs in identity account

--- accounts/identity/vpcs.tf
+++ accounts/identity/vpcs.tf
@@ -1,6 +1,13 @@
-module "identity_vpc" {
+module "identity_vpc_prod" {
   source = "../modules/vpc"
 
-  vpc_name   = "identity-services"
+  vpc_name   = "identity-services-prod"
   cidr_block = "172.72.0.0/16"
+}
+
+module "identity_vpc_stage" {
+  source = "../modules/vpc"
+
+  vpc_name   = "identity-services-stage"
+  cidr_block = "172.73.0.0/16"
 }
