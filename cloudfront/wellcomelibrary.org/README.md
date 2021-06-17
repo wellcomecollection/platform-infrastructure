4023a0da158e277d34d0eb9077e4e1e1321d013f
James Gorrie <j.gorrie@wellcome.org>
2021-06-17 14:28:15 +0100

    use http for archives

--- cloudfront/wellcomelibrary.org/README.md
+++ cloudfront/wellcomelibrary.org/README.md
@@ -5,3 +5,9 @@ CloudFront distributions & edge-lambdas for managing the redirection of results 
 ## AWS Route53 
 
 If you need access to the Route53 console use [this link](https://console.aws.amazon.com/route53/v2/hostedzones?#ListRecordSets/Z78J6G8RSOLSZ). You will need to have permissions to assume the role: `arn:aws:iam::267269328833:role/wellcomecollection-assume_role_hosted_zone_update`.
+
+## Deployment
+```bash
+./deploy
+```
+
--- cloudfront/wellcomelibrary.org/terraform/dserve.tf
+++ cloudfront/wellcomelibrary.org/terraform/dserve.tf
@@ -1,25 +1,26 @@
 // DServe links (archives.wellcomelibrary.org)
 
-//module "wellcomelibrary_dserve-prod" {
-//  source = "./cloudfront_distro"
-//
-//  distro_alternative_names = [
-//    "archives.wellcomelibrary.org"
-//  ]
-//  acm_certificate_arn = module.cert-stage.arn
-//
-//  origins = [{
-//    origin_id : "origin"
-//    domain_name : "archives.origin.wellcomelibrary.org"
-//    origin_path : null
-//  }]
-//
-//  default_target_origin_id                       = "origin"
-//  default_lambda_function_association_event_type = "origin-request"
-//  default_lambda_function_association_lambda_arn = local.wellcome_library_passthru_arn_prod
-//  default_forwarded_headers                      = ["Host"]
-//}
-//
+module "wellcomelibrary_dserve-prod" {
+  source = "./cloudfront_distro"
+
+  distro_alternative_names = [
+    "archives.wellcomelibrary.org"
+  ]
+  acm_certificate_arn = module.cert-stage.arn
+
+  origins = [{
+    origin_id : "origin"
+    domain_name : "archives.origin.wellcomelibrary.org"
+    origin_path : null
+    origin_protocol_policy : "match-viewer"
+  }]
+
+  default_target_origin_id                       = "origin"
+  default_lambda_function_association_event_type = "origin-request"
+  default_lambda_function_association_lambda_arn = local.wellcome_library_archive_redirect_arn_prod
+  default_forwarded_headers                      = ["Host"]
+}
+
 module "wellcomelibrary_dserve-stage" {
   source = "./cloudfront_distro"
 
@@ -41,23 +42,23 @@ module "wellcomelibrary_dserve-stage" {
   default_forwarded_headers                      = ["Host"]
 }
 
-resource "aws_route53_record" "dserve-prod" {
-  zone_id = data.aws_route53_zone.zone.id
-  name    = "archives.wellcomelibrary.org"
-  type    = "CNAME"
-
-  records = ["archives.wellcome.ac.uk."]
-  ttl     = "300"
-
-  provider = aws.dns
-}
-
 resource "aws_route53_record" "dserve-origin" {
   zone_id = data.aws_route53_zone.zone.id
   name    = "archives.origin.wellcomelibrary.org"
   type    = "CNAME"
 
   records = ["archives.wellcome.ac.uk."]
+  ttl     = "60"
+
+  provider = aws.dns
+}
+
+resource "aws_route53_record" "dserve-prod" {
+  zone_id = data.aws_route53_zone.zone.id
+  name    = "archives.wellcomelibrary.org"
+  type    = "CNAME"
+
+  records = [module.wellcomelibrary_dserve-prod.distro_domain_name]
   ttl     = "60"
 
   provider = aws.dns
