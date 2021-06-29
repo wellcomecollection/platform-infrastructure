625ce332a7231c6cb22ab1768f9f2cc801f60b2b
Alex Chan <a.chan@wellcome.org>
2021-06-29 17:47:01 +0100

    okay but properly

--- images/terraform/locals.tf
+++ images/terraform/locals.tf
@@ -7,6 +7,7 @@ locals {
     "130871440101", // experience_account_id
     "653428163053", // digirati_account_id
     "770700576653", // identity_account_id
+    "241906670800",  # dams prototype account
   ]
 
   namespace = "uk.ac.wellcome"
