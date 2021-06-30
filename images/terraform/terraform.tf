6ff5d918a2826a2c04e251946785ff46bb005aa6
Alex Chan <a.chan@wellcome.org>
2021-06-30 10:27:04 +0100

    Bump to the latest version of the AWS provider

--- images/terraform/terraform.tf
+++ images/terraform/terraform.tf
@@ -16,8 +16,7 @@ data "aws_caller_identity" "current" {}
 #Providers
 
 provider "aws" {
-  region  = var.aws_region
-  version = "~> 2.47.0"
+  region = var.aws_region
 
   assume_role {
     role_arn = "arn:aws:iam::760097843905:role/platform-developer"
