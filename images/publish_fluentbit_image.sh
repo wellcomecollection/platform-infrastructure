07bc0722f9e0fc8ef3e2973ffbea627633fa4178
Alex Chan <a.chan@wellcome.org>
2021-06-30 11:31:42 +0100

    Publish API Gateway nginx images to ECR public

--- images/publish_fluentbit_image.sh
+++ images/publish_fluentbit_image.sh
@@ -16,6 +16,21 @@ echo "*** WARNING! ***"
 echo ""
 read -p "Press enter to continue."
 
+
+echo ""
+echo "*** Logging in to ECR Private ***"
+echo ""
+AWS_PROFILE=platform-developer aws ecr get-login --no-include-email | bash
+
+echo ""
+echo "*** Logging in to ECR Public ***"
+echo ""
+
+# We have to log in with us-east-1 because that's where ECR Public lives.
+# See https://docs.aws.amazon.com/AmazonECR/latest/public/getting-started-cli.html#cli-authenticate-registry
+AWS_PROFILE=platform-developer aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+
+
 echo ""
 echo "*** Building fluentbit image ***"
 echo ""
@@ -26,10 +41,8 @@ TAG="fluentbit:$CURRENT_COMMIT"
 docker build --tag="$TAG" fluentbit
 
 echo ""
-echo "*** Publishing fluentbit image to ecr-private ***"
+echo "*** Publishing fluentbit image to ECR Private ***"
 echo ""
-
-AWS_PROFILE=platform-developer aws ecr get-login --no-include-email | bash
 
 ECR_PRIVATE_TAG="$ECR_PRIVATE_PREFIX/fluentbit:$CURRENT_COMMIT"
 
@@ -38,12 +51,8 @@ docker push "$ECR_PRIVATE_TAG"
 docker rmi "$ECR_PRIVATE_TAG"
 
 echo ""
-echo "*** Publishing fluentbit image to ecr-public ***"
+echo "*** Publishing fluentbit image to ECR Public ***"
 echo ""
-
-# We have to log in with us-east-1 because that's where ECR Public lives.
-# See https://docs.aws.amazon.com/AmazonECR/latest/public/getting-started-cli.html#cli-authenticate-registry
-AWS_PROFILE=platform-developer aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
 
 ECR_PUBLIC_TAG="$ECR_PUBLIC_PREFIX/fluentbit:$CURRENT_COMMIT"
 
--- images/publish_nginx_images.sh
+++ images/publish_nginx_images.sh
@@ -4,7 +4,11 @@ set -o nounset
 
 DEV_ROLE_ARN="arn:aws:iam::760097843905:role/platform-developer"
 ROOT=$(git rev-parse --show-toplevel)
-SERVICE_IDS=$(find "$ROOT"/containers/nginx/*.Dockerfile  -type f -exec basename {} \; |  cut -d '.' -f 1)
+
+ECR_PRIVATE_PREFIX="760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome"
+ECR_PUBLIC_PREFIX="public.ecr.aws/l7a1d1z4"
+
+SERVICE_IDS="apigw experience grafana"
 
 echo ""
 echo "*** WARNING! ***"
@@ -14,28 +18,55 @@ echo "*** WARNING! ***"
 echo ""
 read -p "Press enter to continue."
 
+
+
+echo ""
+echo "*** Logging in to ECR Private ***"
+echo ""
+AWS_PROFILE=platform-developer aws ecr get-login --no-include-email | bash
+
+echo ""
+echo "*** Logging in to ECR Public ***"
+echo ""
+
+# We have to log in with us-east-1 because that's where ECR Public lives.
+# See https://docs.aws.amazon.com/AmazonECR/latest/public/getting-started-cli.html#cli-authenticate-registry
+AWS_PROFILE=platform-developer aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+
+
+
 for SERVICE_ID in $SERVICE_IDS
 do
 
-echo ""
-echo "*** Publishing $SERVICE_ID ***"
-echo ""
+  echo ""
+  echo "*** Publishing $SERVICE_ID ***"
+  echo ""
 
-"$ROOT"/scripts/docker_run.py \
-      --dind --root -- \
-      wellcome/image_builder:23 \
-            --project=nginx_"$SERVICE_ID" \
-            --file=images/nginx/"${SERVICE_ID}".Dockerfile
+  CURRENT_COMMIT=$(git rev-parse HEAD)
+  TAG="nginx_$SERVICE_ID:$CURRENT_COMMIT"
 
-"$ROOT"/scripts/docker_run.py \
-      --aws --root --dind -- \
-      wellcome/publish_service:86 \
-        --service_id=nginx_"$SERVICE_ID" \
-          --project_id=platform \
-          --account_id=760097843905 \
-          --region_id=eu-west-1 \
-          --namespace=uk.ac.wellcome \
-          --role_arn="$DEV_ROLE_ARN" \
-          --label=latest
+  docker build --tag="$TAG" --file "nginx/$SERVICE_ID.Dockerfile" nginx
+
+  echo ""
+  echo "*** Publishing $SERVICE_ID image to ECR Private ***"
+  echo ""
+
+  ECR_PRIVATE_TAG="$ECR_PRIVATE_PREFIX/nginx_$SERVICE_ID:$CURRENT_COMMIT"
+
+  docker tag "$TAG" "$ECR_PRIVATE_TAG"
+  docker push "$ECR_PRIVATE_TAG"
+  docker rmi "$ECR_PRIVATE_TAG"
+
+  if [[ "$SERVICE_ID" == "apigw" ]]
+  then
+    echo ""
+    echo "*** Publishing $SERVICE_ID image to ECR Public ***"
+    echo ""
+
+    ECR_PUBLIC_TAG="$ECR_PUBLIC_PREFIX/nginx_$SERVICE_ID:$CURRENT_COMMIT"
+
+    docker tag "$TAG" "$ECR_PUBLIC_TAG"
+    docker push "$ECR_PUBLIC_TAG"
+    docker rmi "$ECR_PUBLIC_TAG"
+  fi
 done
-
