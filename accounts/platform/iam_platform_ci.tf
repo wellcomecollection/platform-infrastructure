37e8ccc2ff65ce5d9e23a652d924165d18ba0cef
Robert Kenny <kenoir@gmail.com>
2021-07-02 09:21:50 +0100

    Allow the CI role to assume itself by default

--- accounts/platform/iam_platform_ci.tf
+++ accounts/platform/iam_platform_ci.tf
@@ -33,25 +33,4 @@ data "aws_iam_policy_document" "platform_ci" {
       "arn:aws:s3:::releases.mvn-repo.wellcomecollection.org/weco/source_model_typesafe_2.12/*",
     ]
   }
-
-  # This slightly unusual clause allows the platform-ci role to assume… itself.
-  #
-  # This is because Buildkite uses the platform-ci role to run tasks
-  # in CI for https://github.com/wellcomecollection/identity, and those CI
-  # tasks in turn run Terraform, which has an "assume_role" block for the
-  # provider (to match our other Terraform configurations).
-  #
-  # When Buildkite running as platform-ci tries to assume platform-ci, that
-  # isn't allowed by default -- we need to explicitly allow it.
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    resources = [
-      "arn:aws:iam::${local.account_ids.platform}:role/platform-ci",
-    ]
-  }
 }
