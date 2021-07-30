55159c9fa7aefe16e12d77b83ec324ecf5edefe1
Alex Chan <a.chan@wellcome.org>
2021-07-30 14:28:02 +0100

    Add an initial Lambda to alert on DLQs

---
+++ monitoring/dlq_to_slack_alerts/src/dlq_to_slack_alerts.py
@@ -0,0 +1,59 @@
+"""
+Sends a notification to Slack when we see a message on a DLQ.
+"""
+
+import httplib
+import json
+import os
+
+import boto3
+
+
+def count_messages_on_queue(queue_name):
+    sqs_client = boto3.client("sqs")
+
+    queue_url = sqs_client.get_queue_url(QueueName=queue_name)
+
+    resp = sqs_client.get_queue_attributes(
+        QueueUrl=queue_url, AttributeNames=["ApproximateNumberOfMessages"]
+    )
+
+    return int(resp["Attributes"]["ApproximateNumberOfMessages"])
+
+
+def main(event, _ctxt=None):
+    alarm = json.loads(event["Records"][0]["Sns"]["Message"])
+    webhook_url = os.environ["SLACK_WEBHOOK"]
+
+    alarm_name = alarm["AlarmName"]
+
+    assert alarm_name.endswith("_dlq_not_empty")
+    queue_name = alarm_name[: -len("_dlq_not_empty")]
+
+    queue_length = count_messages_on_queue(f"{queue_name}_dlq")
+
+    if queue_length == 1:
+        message = f"There is 1 item on the {queue_name} DLQ."
+    else:
+        message = f"There are {queue_length} items on the {queue_name} DLQ."
+
+    slack_payload = {
+        "username": "cloudwatch-warning",
+        "icon_emoji": ":warning:",
+        "attachments": [
+            {
+                "color": "warning",
+                "fallback": alarm_name,
+                "title": alarm_name,
+                "fields": [{"value": message}],
+            }
+        ],
+    }
+
+    print("Sending message %s" % json.dumps(slack_payload))
+
+    conn = httplib.HTTPConnection(webhook_url)
+    conn.request(
+        "POST", "", json.dumps(slack_payload), {"Content-Type": "application/json"}
+    )
+    conn.getresponse()
