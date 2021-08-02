15a9446ba01de29180f3757a4349d77ed0e4abd2
Alex Chan <alex@alexwlchan.net>
2021-08-02 11:25:06 +0100

    Use a published version of the TF module

--- critical/modules/secrets/distributed/main.tf
+++ critical/modules/secrets/distributed/main.tf
@@ -1,5 +1,5 @@
 module "secrets" {
-  source = "github.com/wellcomecollection/terraform-aws-secrets.git?ref=8bf3759af9e1a19731f28784953dd0a53a2f276f"
+  source = "github.com/wellcomecollection/terraform-aws-secrets.git?ref=v1.3.0"
 
   key_value_map = local.name_value_map
 }
