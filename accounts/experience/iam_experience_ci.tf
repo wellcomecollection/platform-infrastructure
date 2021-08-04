2e716242e91241eb0850b9ce6cd46e92a5a89202
jamie <j.parkinson@wellcome.ac.uk>
2021-08-04 17:33:20 +0100

    Allow CI to access secrets for the weco github app

--- accounts/experience/iam_experience_ci.tf
+++ accounts/experience/iam_experience_ci.tf
@@ -1,6 +1,17 @@
 resource "aws_iam_role_policy" "experience_ci" {
   role   = module.experience_account.ci_role_name
-  policy = data.aws_iam_policy_document.experience_ci.json
+  policy = data.aws_iam_policy_document.experience_ci_combined.json
+}
+
+data "aws_iam_policy_document" "experience_ci_combined" {
+  source_policy_documents = [
+    module.github_deployment_secrets.secret_access_policy_document_json,
+    data.aws_iam_policy_document.experience_ci.json
+  ]
+}
+
+module "github_deployment_secrets" {
+  source = "git::github.com/wellcomecollection/github-deployments-buildkite-plugin.git//secrets-tf?ref=v0.2.4"
 }
 
 data "aws_iam_policy_document" "experience_ci" {
@@ -26,6 +37,8 @@ data "aws_iam_policy_document" "experience_ci" {
   }
 
   statement {
+    sid = "GetBuildSecrets"
+
     actions = [
       "secretsmanager:GetSecretValue",
     ]
@@ -36,6 +49,8 @@ data "aws_iam_policy_document" "experience_ci" {
   }
 
   statement {
+    sid = "GetApiSecrets"
+
     actions = [
       "secretsmanager:GetSecretValue",
     ]
