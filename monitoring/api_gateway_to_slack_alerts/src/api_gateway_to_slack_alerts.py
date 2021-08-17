0e14f548ab81bbab6fa824d9594f00c48930a457
Alex Chan <a.chan@wellcome.org>
2021-08-17 11:32:56 +0100

    keep tweaking the post to slack lambda

--- monitoring/api_gateway_to_slack_alerts/src/api_gateway_to_slack_alerts.py
+++ monitoring/api_gateway_to_slack_alerts/src/api_gateway_to_slack_alerts.py
@@ -34,12 +34,7 @@ def get_secret_string(*, secret_id):
     return secrets_client.get_secret_value(SecretId=secret_id)["SecretString"]
 
 
-@log_on_error
-def main(event, _ctxt=None):
-    account = os.environ["ACCOUNT_NAME"]
-
-    alarm = json.loads(event["Records"][0]["Sns"]["Message"])
-
+def create_message(alarm):
     # This will be a message of the form:
     #
     #     Threshold Crossed: 1 datapoint [2.0 (17/08/21 09:08:00)] was
@@ -48,33 +43,41 @@ def main(event, _ctxt=None):
     state_reason = alarm["NewStateReason"]
     error_count = re.search(r'\[(?P<count>\d+)\.0 \(\d{2}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}\)\]', state_reason).group("count")
 
-    print(error_count)
+    if error_count == 1:
+        return "There was a error from API Gateway"
+    else:
+        return f"There were {error_count} errors from API Gateway"
 
-    from pprint import pprint
-    pprint(alarm)
+
+@log_on_error
+def main(event, _ctxt=None):
+    account = os.environ["ACCOUNT_NAME"]
+
+    alarm = json.loads(event["Records"][0]["Sns"]["Message"])
+
     webhook_url = get_secret_string(secret_id="monitoring/critical_slack_webhook")
 
     alarm_name = alarm["AlarmName"]
 
-    # slack_payload = {
-    #     "username": f"{account}-api-gateway-5xx-alarm",
-    #     "icon_emoji": ":warning:",
-    #     "attachments": [
-    #         {
-    #             "color": "warning",
-    #             "fallback": alarm_name,
-    #             "title": alarm_name,
-    #             "fields": [{"value": create_message(alarm_name)}],
-    #         }
-    #     ],
-    # }
-    #
-    # print("Sending message %s" % json.dumps(slack_payload))
-    #
-    # req = urllib.request.Request(
-    #     webhook_url,
-    #     data=json.dumps(slack_payload).encode("utf8"),
-    #     headers={"Content-Type": "application/json"},
-    # )
-    # resp = urllib.request.urlopen(req)
-    # assert resp.status == 200, resp
+    slack_payload = {
+        "username": f"{account}-api-gateway-5xx-alarm",
+        "icon_emoji": ":rotating_light:",
+        "attachments": [
+            {
+                "color": "danger",
+                "fallback": alarm_name,
+                "title": alarm_name,
+                "fields": [{"value": create_message(alarm)}],
+            }
+        ],
+    }
+
+    print("Sending message %s" % json.dumps(slack_payload))
+
+    req = urllib.request.Request(
+        webhook_url,
+        data=json.dumps(slack_payload).encode("utf8"),
+        headers={"Content-Type": "application/json"},
+    )
+    resp = urllib.request.urlopen(req)
+    assert resp.status == 200, resp
