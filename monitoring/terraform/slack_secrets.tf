3365f1439d068bdffa78ef4a7074f0802dbbcb39
Alex Chan <a.chan@wellcome.org>
2021-08-17 11:39:33 +0100

    copy secrets into the identity account

--- monitoring/terraform/slack_secrets.tf
+++ monitoring/terraform/slack_secrets.tf
@@ -21,3 +21,15 @@ module "storage_slack_secrets" {
     aws = aws.storage
   }
 }
+
+module "identity_slack_secrets" {
+  source = "../../critical/modules/secrets/distributed"
+
+  secrets = {
+    noncritical_slack_webhook = "monitoring/critical_slack_webhook"
+  }
+
+  providers = {
+    aws = aws.identity
+  }
+}
