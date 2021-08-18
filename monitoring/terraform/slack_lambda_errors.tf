ee83f6a01f3fef899a6057e245beb74f3f09a598
Alex Chan <a.chan@wellcome.org>
2021-08-18 09:44:04 +0100

    wire up the alert-on-lambda error lambda to… itself

--- monitoring/terraform/slack_lambda_errors.tf
+++ monitoring/terraform/slack_lambda_errors.tf
@@ -1,5 +1,5 @@
 module "platform_lambda_error_alerts" {
-  source = "./modules/slack_lambda_error_alerts"
+  source = "./modules/slack_alert_on_lambda_error"
 
   providers = {
     aws = aws.platform
@@ -8,7 +8,7 @@ module "platform_lambda_error_alerts" {
   account_name = "platform"
   infra_bucket = "wellcomecollection-platform-infra"
 
-  alarm_topic_arn = local.lambda_error_alarm_arn
+  alarm_topic_arn = module.platform_lambda_error_alerts.alarm_topic_arn
 }
 
 output "platform_lambda_error_alerts_topic_arn" {
