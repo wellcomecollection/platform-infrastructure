64e2bb7b632b5e6abcf07e360ef284e6758aeb89
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-08-25 10:24:50 +0000

    Apply auto-formatting rules

--- .buildkite/scripts/run_autoformat.py
+++ .buildkite/scripts/run_autoformat.py
@@ -18,21 +18,27 @@ from provider import current_branch, repo
 
 if __name__ == "__main__":
     root = git("rev-parse", "--show-toplevel")
-    home = os.environ['HOME']
+    home = os.environ["HOME"]
 
-    subprocess.check_call(f"""
+    subprocess.check_call(
+        f"""
         docker run --tty --rm \
             --volume {os.path.join(home, '.aws')}:/root/.aws \
             --volume {root}:/repo \
             --workdir /repo \
             760097843905.dkr.ecr.eu-west-1.amazonaws.com/hashicorp/terraform:light fmt -recursive
-    """.strip(), shell=True)
+    """.strip(),
+        shell=True,
+    )
 
-    subprocess.check_call(f"""
+    subprocess.check_call(
+        f"""
         docker run --tty --rm \
             --volume {root}:/repo \
             760097843905.dkr.ecr.eu-west-1.amazonaws.com/wellcome/format_python:112
-    """.strip(), shell=True)
+    """.strip(),
+        shell=True,
+    )
 
     # If there are any changes, push to GitHub immediately and fail the
     # build.  This will abort the remaining jobs, and trigger a new build
@@ -62,10 +68,13 @@ if __name__ == "__main__":
     # Run the 'lint' tasks.  A failure in these tasks requires
     # manual intervention, so we run them second to get any automatic fixes
     # out of the way.
-    subprocess.check_call(f"""
+    subprocess.check_call(
+        f"""
         docker run --tty --rm \
             --volume {root}:/repo \
             --workdir /repo \
             760097843905.dkr.ecr.eu-west-1.amazonaws.com/wellcome/flake8:latest \
             --exclude .git,__pycache__,target,.terraform --ignore=E501,E122,E126,E203,W503
-    """.strip(), shell=True)
+    """.strip(),
+        shell=True,
+    )
