3f8647a3d8f4882409c2718c6a92a4e00ecd5c98
Alex Chan <a.chan@wellcome.org>
2021-12-01 16:45:56 +0000

    Try to sort out S3 ACLs

--- critical/locals.tf
+++ critical/locals.tf
@@ -2,8 +2,9 @@ locals {
   aws_region = "eu-west-1"
 
   account_ids = {
-    catalogue = "756629837203"
-    reporting = "269807742353"
+    catalogue  = "756629837203"
+    reporting  = "269807742353"
+    experience = "130871440101"
   }
 
   default_tags = {
--- critical/s3_cloudfront_logs.tf
+++ critical/s3_cloudfront_logs.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "api_root_cf_logs" {
   bucket = "weco-cloudfront-logs"
-  acl    = "private"
+
+  policy = data.aws_iam_policy_document.s3_cloudfront_logs.json
 
   lifecycle_rule {
     id      = "cf-logs"
@@ -9,6 +10,35 @@ resource "aws_s3_bucket" "api_root_cf_logs" {
 
     expiration {
       days = 30
+    }
+  }
+
+  grant {
+    # Grant CloudFront logs access to your Amazon S3 Bucket
+    # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#AccessLogsBucketAndFileOwnership
+    id          = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+}
+
+data "aws_iam_policy_document" "s3_cloudfront_logs" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::weco-cloudfront-logs",
+      "arn:aws:s3:::weco-cloudfront-logs/*"
+    ]
+
+    principals {
+      identifiers = [
+        "arn:aws:iam::${local.account_ids["experience"]}:root"
+      ]
+
+      type = "AWS"
     }
   }
 }
