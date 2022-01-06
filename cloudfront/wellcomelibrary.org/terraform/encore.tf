35350fdcaee222abc2ba8a9f68362e4a29724977
Alex Chan <a.chan@wellcome.org>
2022-01-06 14:41:27 +0000

    Set up a CloudFront distro for search.wellcomelibrary.org

--- cloudfront/wellcomelibrary.org/terraform/encore.tf
+++ cloudfront/wellcomelibrary.org/terraform/encore.tf
@@ -1,25 +1,26 @@
 // Encore links (search.wellcomelibrary.org)
 
-//module "wellcomelibrary_encore-prod" {
-//  source = "./cloudfront_distro"
-//
-//  distro_alternative_names = [
-//    "archives.wellcomelibrary.org"
-//  ]
-//  acm_certificate_arn = module.cert-stage.arn
-//
-//  origins = [{
-//    origin_id : "origin"
-//    domain_name : "archives.origin.wellcomelibrary.org"
-//    origin_path : null
-//  }]
-//
-//  default_target_origin_id                       = "origin"
-//  default_lambda_function_association_event_type = "origin-request"
-//  default_lambda_function_association_lambda_arn = local.wellcome_library_passthru_arn_prod
-//  default_forwarded_headers                      = ["Host"]
-//}
-//
+module "wellcomelibrary_encore-prod" {
+  source = "./cloudfront_distro"
+
+  distro_alternative_names = [
+    "search.wellcomelibrary.org"
+  ]
+  acm_certificate_arn = module.cert-stage.arn
+
+  origins = [{
+    origin_id : "origin"
+    domain_name : "search.origin.wellcomelibrary.org"
+    origin_path : null
+    origin_protocol_policy : "http-only"
+  }]
+
+  default_target_origin_id                       = "origin"
+  default_lambda_function_association_event_type = "origin-request"
+  default_lambda_function_association_lambda_arn = local.wellcome_library_passthru_arn_prod
+  default_forwarded_headers                      = ["Host"]
+}
+
 module "wellcomelibrary_encore-stage" {
   source = "./cloudfront_distro"
 
@@ -46,8 +47,8 @@ resource "aws_route53_record" "encore-prod" {
   name    = "search.wellcomelibrary.org"
   type    = "A"
 
-  records = ["35.176.25.168"]
-  ttl     = "300"
+  records = ["35.176.25.168"] # [module.wellcomelibrary_encore-stage.distro_domain_name]
+  ttl     = "60"
 
   provider = aws.dns
 }
