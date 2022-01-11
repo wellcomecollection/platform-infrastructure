f742e5d93d80b378ad0c4afc89fe6d65d2e83928
Alex Chan <a.chan@wellcome.org>
2022-01-11 09:45:27 +0000

    Use shared logic for parsing Encore URL components

--- cloudfront/wellcomelibrary.org/edge-lambda/src/wellcomeLibraryEncoreRedirect.ts
+++ cloudfront/wellcomelibrary.org/edge-lambda/src/wellcomeLibraryEncoreRedirect.ts
@@ -10,26 +10,72 @@ import {
 import { calcCheckDigit, GetBNumberResult } from './paths';
 import querystring from 'querystring';
 
+type EncorePathComponent = {
+  letter: string;
+  contents?: string;
+};
+
+// Parses the final path part of an Encore URL into 'components'.
+//
+// I'm having to guess at how Encore URLs work here -- it looks like the
+// final part is a collection of components separated by a double underscore, and
+// the first letter of each component tells you waht that component is.
+// e.g. if we look at a more complex URL:
+//
+//    /iii/encore/record/C__Rb3153458__Sdrugscope__P0%2C1__Orightresult__U__X7
+//
+// Then we have:
+//
+//    R = record = b3153458
+//    S = search = drugscope
+//    P = ???    = 0,1
+//    O = ???    = rightresult
+//    U = ???    = <empty>
+//    X = ???    = 7
+//
+// and for these URLs, we want the component that starts with 'S'.
+//
+// This function takes the part after the final slash.
+//
+// Note: components are returned as they appear in the URL; this function does *not*
+// do any URL decoding.
+//
+function parseEncorePathComponents(finalPathPart: string): EncorePathComponent[] {
+  return finalPathPart.split('__')
+    .map(component => {
+      const letter = component.substring(0, 1);
+      const contents = component.substring(1, );
+      
+      return {
+        letter: letter,
+        contents: contents.length > 0 ? contents : undefined,
+      };
+    })
+}
+
 export function getBnumberFromEncorePath(path: string): GetBNumberResult {
   if (!path.startsWith('/iii/encore/record/')) {
     return Error(`Path ${path} does not start with /iii/encore/record/`);
   }
-  const pathPath = path.split('/')[4];
 
-  const sierraIdRegexp = /^C__Rb([0-9]{7})/;
+  const finalPathPart = path.split('/')[4];
+  const components = parseEncorePathComponents(finalPathPart);
+  
+  const sierraBibRegexp = /^b([0-9]{7})$/;
 
-  if (!sierraIdRegexp.test(pathPath)) {
-    return Error(`b number in ${path} (${pathPath}) does not match ${sierraIdRegexp}`);
+  // If defined, this will be something like '1234567'
+  const bnumber = components
+    .filter(c => c.letter == 'R')
+    .find(c => c.contents && sierraBibRegexp.test(c.contents))
+    ?.contents?.substring(1, );
+
+  if (!bnumber) {
+    return Error(`Could not find bib identifier in path ${path}`);
   }
 
-  const sierraIdentifier = pathPath.toLowerCase().substring(5, 12);
-  const sierraSystemNumber = `b${sierraIdentifier}${calcCheckDigit(
-    parseInt(sierraIdentifier)
-  )}`;
-
   return {
-    sierraIdentifier: sierraIdentifier,
-    sierraSystemNumber: sierraSystemNumber,
+    sierraIdentifier: bnumber,
+    sierraSystemNumber: `b${bnumber}${calcCheckDigit(parseInt(bnumber))}`,
   };
 }
 
@@ -55,38 +101,15 @@ function getSearchRedirect(
   }
 
   // For URLs like /iii/encore/search/C__Srosalind%20paget
-  //
-  // I'm having to guess at how Encore URLs work here -- it looks like the
-  // final part is a collection of components separated by a double underscore, and
-  // the first letter of each component tells you waht that component is.
-  // e.g. if we look at a more complex URL:
-  //
-  //    /iii/encore/record/C__Rb3153458__Sdrugscope__P0%2C1__Orightresult__U__X7
-  //
-  // Then we have:
-  //
-  //    R = record = b3153458
-  //    S = search = drugscope
-  //    P = ???    = 0,1
-  //    O = ???    = rightresult
-  //    U = ???    = <empty>
-  //    X = ???    = 7
-  //
-  // and for these URLs, we want the component that starts with 'S'.
-  const parts = path.split('/');
-  if (parts.length === 5) {
-    const finalPart = parts[4];
-    const searchComponent =
-      finalPart
-        .split('__')
-        .find(c => c.startsWith('S'));
+  const finalPathPart = path.split('/')[4];
+  const components = parseEncorePathComponents(finalPathPart);
 
-    // Remember to strip the leading 'S'
-    const searchTerms = searchComponent?.substring(1, );
+  const searchTerms = components
+    .find(c => c.letter == 'S')
+    ?.contents;
 
-    if (searchTerms) {
-      return wellcomeCollectionRedirect(`/works?query=${searchTerms}`);
-    }
+  if (searchTerms) {
+    return wellcomeCollectionRedirect(`/works?query=${searchTerms}`);
   }
 
   // If we've matched nothing we redirect to the top-level collections page
