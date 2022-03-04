5aea653665cacee7d57088fa9604b6d904140dc2
Alex Chan <alex@alexwlchan.net>
2022-03-04 15:09:07 +0000

    Remove a now-unused S3 bucket

--- monitoring/terraform/s3.tf
+++ monitoring/terraform/s3.tf
@@ -20,32 +20,3 @@ resource "aws_s3_bucket" "dashboard" {
     }
   }
 }
-
-resource "aws_s3_bucket" "monitoring" {
-  bucket = "wellcomecollection-platform-monitoring"
-  acl    = "private"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  lifecycle_rule {
-    id      = "gatling"
-    prefix  = "gatling/"
-    enabled = true
-
-    expiration {
-      days = 30
-    }
-  }
-
-  lifecycle_rule {
-    id      = "terraform_plans"
-    prefix  = "terraform_plans/"
-    enabled = true
-
-    expiration {
-      days = 30
-    }
-  }
-}
