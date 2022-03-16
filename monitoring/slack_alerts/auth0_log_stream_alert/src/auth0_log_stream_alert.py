e0f74444d38ff78c419b59a2330868f2d91c3f46
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-03-16 17:42:27 +0000

    Apply auto-formatting rules

--- monitoring/slack_alerts/auth0_log_stream_alert/src/auth0_log_stream_alert.py
+++ monitoring/slack_alerts/auth0_log_stream_alert/src/auth0_log_stream_alert.py
@@ -21,6 +21,7 @@ import urllib.request
 
 import boto3
 
+
 def log_on_error(fn):
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
@@ -32,6 +33,7 @@ def log_on_error(fn):
             raise
 
     return wrapper
+
 
 def get_secret_string(*, secret_id):
     """
@@ -53,7 +55,9 @@ def get_log_url(log_id, *, tenant_name):
 @log_on_error
 def main(event, _ctxt=None):
     webhook_url = get_secret_string(secret_id="monitoring/critical_slack_webhook")
-    log_event = json.loads(event["Records"][0]["Sns"]["Message"])  # There will only ever be 1 record
+    log_event = json.loads(
+        event["Records"][0]["Sns"]["Message"]
+    )  # There will only ever be 1 record
 
     if not should_alert_for_event(log_event):
         return
@@ -73,16 +77,16 @@ def main(event, _ctxt=None):
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": '\n'.join([short_message, message_content])
-                }
+                    "text": "\n".join([short_message, message_content]),
+                },
             }
-        ]
+        ],
     }
 
     req = urllib.request.Request(
         webhook_url,
         data=json.dumps(slack_payload).encode("utf8"),
-        headers={"Content-Type": "application/json"}
+        headers={"Content-Type": "application/json"},
     )
     resp = urllib.request.urlopen(req)
     assert resp.status == 200, resp
