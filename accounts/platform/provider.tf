bb567372d4dd15e93460dcff6e69657ac5810d1e
Alex Chan <alex@alexwlchan.net>
2022-06-14 08:01:55 +0100

    Tag all the resources in this configuration

--- accounts/platform/provider.tf
+++ accounts/platform/provider.tf
@@ -1,7 +1,17 @@
+locals {
+  default_tags = {
+    TerraformConfigurationURL = "https://github.com/wellcomecollection/platform-infrastructure/tree/main/accounts/platform"
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
 
   assume_role {
     role_arn = "arn:aws:iam::760097843905:role/platform-admin"
+  }
+  
+  default_tags {
+    tags = local.default_tags
   }
 }
