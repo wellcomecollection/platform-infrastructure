af73fdeaf5394ed634ba25635127460ecdbf8f46
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-07-26 18:12:42 +0000

    Apply auto-formatting rules

--- monitoring/slack_alerts/auth0_log_stream_alert/src/auth0_log_stream_alert.py
+++ monitoring/slack_alerts/auth0_log_stream_alert/src/auth0_log_stream_alert.py
@@ -114,9 +114,7 @@ def should_alert_for_event(log_event):
             "PIN is not valid : PIN is trivial",
         ],
         # fs = Failed Signup
-        "fs": [
-            "Password is not allowed, it might be too common.",
-        ],
+        "fs": ["Password is not allowed, it might be too common."],
         # Rate Limit on the Authentication or Management APIs
         "api_limit": ["Global per second default group limit has been reached"],
     }
