a3d972948a0ea4d274872fd9f64b1b2a4d2965f6
Alex Chan <alex@alexwlchan.net>
2022-08-01 09:05:05 +0100

    Provide more information in the Auth0 Slack alerts
    We don't send the description from the log event to Slack because it
    might contain PII, but there is a "log event type code" that we can map
    to a human-readable description.
    
    This should make triage of events slightly easier, by allowing us to
    make an initial assessment before we open the dashboard.

---
+++ monitoring/slack_alerts/auth0_log_stream_alert/src/download_log_event_type_codes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This script creates the JSON file ``log_event_type_codes.json``.
+
+This file is used by the Lambda to interpret the log event type, and
+provide the longer explanation in the Slack message.
+
+You only need to re-run this script if Auth0 add new codes; it's kept
+for posterity, rather than something I expect to be used as part of
+normal operations.
+
+"""
+
+import json
+
+import bs4
+import httpx
+
+if __name__ == "__main__":
+    resp = httpx.get("https://auth0.com/docs/deploy-monitor/logs/log-event-type-codes")
+    resp.raise_for_status()
+
+    html = resp.text
+
+    soup = bs4.BeautifulSoup(html, "html.parser")
+
+    table = soup.find("article").find("table")
+
+    codes = {}
+
+    for row in table.find_all("tr"):
+
+        # header row
+        if row.find("td") is None:
+            continue
+
+        event_code, event, description = row.find_all("td")
+
+        codes[event_code.text] = event.text
+
+    with open("log_event_type_codes.json", "w") as outfile:
+        outfile.write(json.dumps(codes, indent=2, sort_keys=True))
