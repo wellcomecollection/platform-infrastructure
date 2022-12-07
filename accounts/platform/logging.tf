048ca69414350e0184ecfc9c8795d755274a1f96
jamie <j.parkinson@wellcome.ac.uk>
2022-12-07 15:34:45 +0000

    Beep boop I'm a single source of truth

--- accounts/platform/logging.tf
+++ accounts/platform/logging.tf
@@ -14,6 +14,10 @@ module "elastic_log_forwarder" {
   source = "../modules/elastic_log_forwarder"
 
   kinesis_log_stream_arn = aws_kinesis_stream.logs_for_esf.arn
-  es_data_stream         = "service-logs-esf"
+  es_data_stream         = local.es_data_stream
   es_api_key_secret      = "elasticsearch/logging/esf/api_key"
+}
+
+locals {
+  es_data_stream = data.terraform_remote_state.platform_infra_shared.outputs["esf_data_stream_name"]
 }
