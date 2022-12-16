aa8fa921f58e82910f2aa5e0373dcd5211541313
jamie <j.parkinson@wellcome.ac.uk>
2022-12-16 13:24:39 +0000

    Parameter needs to be in every account

--- critical/lambda_logging.tf
+++ critical/lambda_logging.tf
@@ -15,8 +15,70 @@ module "kinesis_log_destination" {
 
 // Put this in parameter store rather than as an output
 // so that our Lambdas don't need access to the whole critical stack state
-resource "aws_ssm_parameter" "log_destination_arn" {
-  name = "/logging/esf/destination_arn"
+locals {
+  esf_destination_parameter_name = "/logging/esf/destination_arn"
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_platform" {
+  provider = aws.platform
+
+  name = local.esf_destination_parameter_name
+  type = "String"
+  value = module.kinesis_log_destination.cloudwatch_log_destination.arn
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_catalogue" {
+  provider = aws.catalogue
+
+  name = local.esf_destination_parameter_name
+  type = "String"
+  value = module.kinesis_log_destination.cloudwatch_log_destination.arn
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_digirati" {
+  provider = aws.digirati
+
+  name = local.esf_destination_parameter_name
+  type = "String"
+  value = module.kinesis_log_destination.cloudwatch_log_destination.arn
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_experience" {
+  provider = aws.experience
+
+  name = local.esf_destination_parameter_name
+  type = "String"
+  value = module.kinesis_log_destination.cloudwatch_log_destination.arn
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_identity" {
+  provider = aws.identity
+
+  name = local.esf_destination_parameter_name
+  type = "String"
+  value = module.kinesis_log_destination.cloudwatch_log_destination.arn
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_reporting" {
+  provider = aws.reporting
+
+  name = local.esf_destination_parameter_name
+  type = "String"
+  value = module.kinesis_log_destination.cloudwatch_log_destination.arn
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_storage" {
+  provider = aws.storage
+
+  name = local.esf_destination_parameter_name
+  type = "String"
+  value = module.kinesis_log_destination.cloudwatch_log_destination.arn
+}
+
+resource "aws_ssm_parameter" "log_destination_arn_workflow" {
+  provider = aws.workflow
+
+  name = local.esf_destination_parameter_name
   type = "String"
   value = module.kinesis_log_destination.cloudwatch_log_destination.arn
 }
