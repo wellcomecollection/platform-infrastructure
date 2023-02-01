769edc7caa464d8cc680358785b079b19400debb
Alex Chan <alex@alexwlchan.net>
2023-02-01 09:52:49 +0000

    Remove a couple of unused variables

--- accounts/identity/locals.tf
+++ accounts/identity/locals.tf
@@ -4,10 +4,5 @@ locals {
     identity = "770700576653"
   }
 
-  aws_region = "eu-west-1"
-
-  secrets_base_arn   = "arn:aws:secretsmanager:${local.aws_region}:${local.account_ids["identity"]}:secret:"
-  ssm_param_base_arn = "arn:aws:ssm:${local.aws_region}:${local.account_ids["identity"]}:parameter/"
-
   account_principals = { for key, value in local.account_ids : key => "arn:aws:iam::${value}:root" }
 }
