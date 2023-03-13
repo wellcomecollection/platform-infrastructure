9c5bf554fce09bcfc30a44af9cd9eba9723224d5
Alex Chan <alex@alexwlchan.net>
2023-03-13 08:42:03 +0000

    Upgrade the account.wellcomecollection.org CloudFront Terraform to the 4.x provider
    There are no S3 resources so no deprecation warnings to fix, but there
    is a local that points to a non-existent output in another stack.  Since
    this is only used in commented-out code, I've commented out the local
    for now -- it can be fixed by anybody who wants to use the commented-out
    code.

--- cloudfront/account.wellcomecollection.org/terraform/locals.tf
+++ cloudfront/account.wellcomecollection.org/terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  account_zone_name_servers = data.terraform_remote_state.identity.outputs.account_zone_name_servers
+  /*account_zone_name_servers = data.terraform_remote_state.identity.outputs.account_zone_name_servers*/
   identity_ses_txt_records  = data.terraform_remote_state.identity.outputs.wellcomecollection_org_ses_vertification_token["records"]
   identity_dkim_cname       = data.terraform_remote_state.identity.outputs.wellcomecollection_org_ses_dkim_tokens
   identity_ses_dkim_records = toset([
