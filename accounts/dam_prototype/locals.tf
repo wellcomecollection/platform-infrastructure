27228bc9c4908fb6c594c25147a734737992b6b2
Alex Chan <alex@alexwlchan.net>
2023-03-13 08:23:52 +0000

    Remove the dam_prototype infrastructure

--- accounts/dam_prototype/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  account_ids = {
-    platform      = "760097843905"
-    dam_prototype = "241906670800"
-  }
-
-  account_principals = { for key, value in local.account_ids : key => "arn:aws:iam::${value}:root" }
-}
