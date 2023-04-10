c517216a49bb3f3d2eda0ef82ed4b104a2d6f703
Alex Chan <alex@alexwlchan.net>
2023-04-10 18:37:49 +0100

    Improve the "ECS tasks can't start" Lambda
    * Link to the ECS console for failing services
    * React to the EventBridge notification, not SNS
    * Add a docstring comment explaining how it works

--- monitoring/slack_alerts/ecs_tasks_cant_start_alert/src/ecs_tasks_cant_start_alert.py
+++ monitoring/slack_alerts/ecs_tasks_cant_start_alert/src/ecs_tasks_cant_start_alert.py
@@ -7,8 +7,58 @@ and leaving the event:
 This helps us spot when, say, we've deployed a bad configuration or we have
 an issue with ECS capacity.
 
-This Lambda is triggered when these events get sent (via a CloudWatch rule),
-so we extract the key information and post a message to Slack.
+== How it works ==
+
+This is the rough architecture:
+
+    +--------------------+
+    | ECS service events |
+    +--------------------+
+              |
+       all service events
+              |
+              v
+    +--------------------+
+    | Amazon EventBridge |
+    +--------------------+
+              |
+ "failed to start" events only"
+              |
+              v
+    +--------------------+
+    |    this Lambda     |
+    +--------------------+
+              |
+         Slack webhook
+
+The EventBridge integration is set up in Terraform.
+
+== Example event ==
+
+Here's an example of the sort of event this function receives:
+
+    {
+      "account": "760097843905",
+      "detail": {
+        "clusterArn": "arn:aws:ecs:eu-west-1:760097843905:cluster/catalogue-2023-03-09",
+        "createdAt": "2023-04-07T07:22:50.331Z",
+        "eventName": "SERVICE_TASK_START_IMPAIRED",
+        "eventType": "WARN"
+      },
+      "detail-type": "ECS Service Action",
+      "id": "b2a64ade-0dfc-adcc-567d-7ffbd225a3c6",
+      "region": "eu-west-1",
+      "resources": [
+        "arn:aws:ecs:eu-west-1:760097843905:service/catalogue-2023-03-09/image_inferrer"
+      ],
+      "source": "aws.ecs",
+      "time": "2023-04-07T07:22:50Z",
+      "version": "0"
+    }
+
+== Deployment ==
+
+This Lambda is deployed by running a Terraform plan/apply.
 
 """
 
@@ -50,46 +100,44 @@ def main(event, _ctxt=None):
     sess = boto3.Session()
 
     account = os.environ["ACCOUNT_NAME"]
+    aws_region = os.environ['AWS_REGION']
 
     webhook_url = get_secret_string(sess, secret_id="monitoring/critical_slack_webhook")
 
-    for record in event["Records"]:
-        ecs_event = json.loads(record["Sns"]["Message"])
+    # The 'resources' list will contain a list of ECS service ARNs, e.g.
+    #
+    #     arn:aws:ecs:eu-west-1:1234567890:service/pipeline/image_inferrer
+    #
+    # Extract the cluster/service name; in this case 'pipeline' and
+    # 'image_inferrer'.
+    for r in event['resources']:
+        _, cluster_name, service_name = r.split("/")
 
-        # The 'resources' list will contain a list of ECS service ARNs, e.g.
-        #
-        #     arn:aws:ecs:eu-west-1:1234567890:service/pipeline/image_inferrer
-        #
-        # Extract the cluster/service name; in this case 'pipeline' and
-        # 'image_inferrer'.
-        for r in ecs_event['resources']:
-            _, cluster_name, service_name = r.split("/")
+        slack_payload = {
+            "username": f"{account}-ecs-tasks-alert",
+            "icon_emoji": ":rotating_light:",
+            "attachments": [
+                {
+                    "color": "danger",
+                    "title": f"{cluster_name} / {service_name}",
+                    "fields": [
+                        {
+                            "value": f"{service_name} is unable to consistently start tasks successfully. <https://{aws_region}.console.aws.amazon.com/ecs/v2/clusters/{cluster_name}/services/{service_name}/deployments?region=eu-west-1%7CView in console>"
+                        }
+                    ],
+                }
+            ],
+        }
 
-            slack_payload = {
-                "username": f"{account}-ecs-tasks-alert",
-                "icon_emoji": ":rotating_light:",
-                "attachments": [
-                    {
-                        "color": "danger",
-                        "title": f"{cluster_name} / {service_name}",
-                        "fields": [
-                            {
-                                "value": f"{service_name} is unable to consistently start tasks successfully."
-                            }
-                        ],
-                    }
-                ],
-            }
+        print("Sending message %s" % json.dumps(slack_payload))
 
-            print("Sending message %s" % json.dumps(slack_payload))
+        req = urllib.request.Request(
+            webhook_url,
+            data=json.dumps(slack_payload).encode("utf8"),
+            headers={"Content-Type": "application/json"},
+        )
 
-            req = urllib.request.Request(
-                webhook_url,
-                data=json.dumps(slack_payload).encode("utf8"),
-                headers={"Content-Type": "application/json"},
-            )
-
-            try:
-                urllib.request.urlopen(req)
-            except HTTPError as err:
-                raise Exception(f"{err} - {err.read()}")
+        try:
+            urllib.request.urlopen(req)
+        except HTTPError as err:
+            raise Exception(f"{err} - {err.read()}")
