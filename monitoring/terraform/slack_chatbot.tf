436b1466499d2f92f370ce1e90a8063c8e9a7cab
Robert Kenny <r.kenny@wellcome.org>
2025-12-02 13:27:25 +0000

    add testing slackbot

--- monitoring/terraform/slack_chatbot.tf
+++ monitoring/terraform/slack_chatbot.tf
@@ -11,10 +11,28 @@ module "slack_chatbot" {
   }
 }
 
+module "slack_chatbot_testing" {
+  source = "./modules/slack_chatbot"
+
+  configuration_name = "testing-alerting"
+  slack_workspace_id = data.aws_ssm_parameter.slack_workspace_id.value
+  slack_channel_id   = data.aws_ssm_parameter.slack_channel_id_testing.value
+
+  providers = {
+    aws.platform   = aws.platform
+    aws.experience = aws.experience
+  }
+}
+
+
 data "aws_ssm_parameter" "slack_workspace_id" {
   name = "/platform/alert_chatbot/workspace_id"
 }
 
 data "aws_ssm_parameter" "slack_channel_id" {
   name = "/platform/alert_chatbot/channel_id"
+}
+
+data "aws_ssm_parameter" "slack_channel_id_testing" {
+  name = "/platform/alert_chatbot/testing_channel_id"
 }
